b03616ef500442385506eb4c24d1f18237311694
dskilly <nskilly@gmail.com>
2025-08-19 19:55:16 -0400

    Finishing the last chapter of the lesson for bookbot project

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import sys
 from stats import get_num_words
 from stats import get_num_chars
 from stats import sort_dict
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = sys.argv[1]
     print("============ BOOKBOT ============")
-    book = "books/frankenstein.txt"
     print(f"Analyzing book found at {book}...")
     print("----------- Word Count ----------")
     num_words = get_num_words(book)
@@ -16,7 +20,7 @@ def main():
     new_list = sort_dict(num_chars)
     for c in new_list:
         if c["char"].isalpha():
-            print(f"{c["char"]}: {c["num"]}")
+            print(f"{c['char']}: {c['num']}")
     print("============= END ===============")
 
 main()
